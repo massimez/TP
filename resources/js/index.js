cf7d37e679bcb219915f3d5b9925d7c757ea9a79
massimez <massi.mez@ya.ru>
2021-03-29 22:13:52 +0700

    Add style to login

---
+++ resources/js/index.js
@@ -0,0 +1,23 @@
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+// export default class Test extends Component{
+//     return (
+//         <div className="container">
+//             <div className="row justify-content-center">
+//                 <div className="col-md-8">
+//                     <div className="card">
+//                         <div className="card-header">Index Component</div>
+
+//                         <div className="card-body">I'm an Index component!</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+
+// if (document.getElementById('test')) {
+//     ReactDOM.render(<Test />, document.getElementById('test'));
+// }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+       <h1>Welcome to the Dashboard</h1>
+       <div>@if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+        @endif</div>
+    </div>
+</div>
+@endsection
