330db4a4b8546540d0ef4282d8c5ff9df1b53171
massimez <massi.mez@ya.ru>
2021-05-16 23:55:32 +0700

    🐛 Fix add import closebutton

--- resources/js/components/ErrorMessage.js
+++ resources/js/components/ErrorMessage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Alert, AlertIcon, AlertDescription } from '@chakra-ui/react';
+import { Box, Alert, AlertIcon, AlertDescription,CloseButton } from '@chakra-ui/react';
 export default function ErrorMessage({ message }) {
   return (
     <Box my={4}>
